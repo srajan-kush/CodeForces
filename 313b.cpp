9ac7c6b2ec8979f1ca4641f79d29c7c74299893a
Srajan Kushwaha <srajank2003@gmail.com>
2025-11-29 20:37:01 +0530

    313-B

--- 313b.cpp
+++ 313b.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 int main(){
     string s;
-    int t,a,b,ans;
+    int t,a,b,ans = 0;
     cin >> s;
+    int n = s.size();
+    vector<int>loby(n,0);
+    for(int i = 1; i < n; i++){
+        if(s[i] == s[i - 1]) ans++;
+        loby[i] = ans;
+    }
     cin >> t;
     while(t--){
         cin >> a >> b;
-        ans = 0;
-        for(int i = a; i < b; i++){
-            if(s[i] == s[i - 1]) ans++;
-        }
-        cout << ans << endl;
+        cout << loby[b - 1] - loby[a - 1] << endl;
     }
     
     return 0;
