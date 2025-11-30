44745e843a876188f09df6fe665ecc10cbd018f8
Srajan Kushwaha <srajank2003@gmail.com>
2025-11-30 18:29:48 +0530

    368-B

---
+++ 327a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    vector<int> arr(t);
+
+
+    for(int i = 0; i < t; i++) cin >> arr[i];
+
+    
+
+    return 0;
+}
